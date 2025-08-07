8cbdd349e0ca085e36f6e5b5026b1b3252f5ec13
Hammad Ali <hammadali78056@gmail.com>
2025-08-07 15:56:54 +0500

    misc: complete the project with practice in redux toolkit

--- todo-redux/src/App.jsx
+++ todo-redux/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1>Learn about Redux Toolkit</h1>
+    <>
+    <h1 className='text-6xl underline font-bold text-blue-600'>Learn About Redux Toolkit</h1>
+    <AddTodo/>
+    <Todos/>
+    <Footer/>
+
+    </>
+
+
   )
 }
 
---
+++ todo-redux/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='text-center py-4'>
+      <p className='text-gray-600'>© 2025 My Todo App</p>
+    </div>
+  )
+}
+
+export default Footer
